2366e7f5caadb52bc0f541840634c4a458933a26
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-09-16 12:53:39 -0600

    subroutines mostly written

--- main.asm
+++ main.asm
@@ -18,11 +18,11 @@
             .retainrefs                     ; Additionally retain any sections
                                             ; that have references to current
                                             ; section
-message:	.byte		0x11, 0x11, 0x11
+message:	.byte		0x00, 0x11, 0x11
 key:		.equ		0xac
 
 			.data
-results:	.space		256					;40 bytes of memory in RAM
+results:	.space		256					;256 bytes of memory in RAM
 			.text							;back to ROM
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
@@ -33,11 +33,14 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 
 initialize:
-	;load key and memory addresses
-	call		decrypt_message
+		;load key and memory addresses
+		mov.b		#message,	R5
+		mov.b		#key,		R6
+		mov.b		#results,	R7
+		call		#decrypt_message
 
 end:
-	jmp			end							;infinite loop
+		jmp			end							;infinite loop
 
 ;---------------------------------------------------
 ;Subroutine Name: decrpyt_message
@@ -48,11 +51,13 @@ end:
 ;Outputs: result in RAM
 ;Registers destroyed:
 ;---------------------------------------------------
-
-decrpyt_message:
+decrypt_message:
     	;load next byte, store in R8
-    	call 		decrypt_byte
+    	mov.b		@R5+,		R8
+    	call 		#decrypt_byte
     	;store result in R8 to RAM
+    	mov.b		R8,			0(R7)
+    	inc.b		R7
     ret
 
 ;---------------------------------------------------
@@ -63,9 +68,9 @@ decrpyt_message:
 ;Outputs: result in R8
 ;Registers destroyed: R8
 ;---------------------------------------------------
-
-decrpyt_byte:
+decrypt_byte:
     	;xor R8 message byte and R6 key
+    	xor.b		R6,			R8
     ret
 
 ;-------------------------------------------------------------------------------
